c90fc4999e5e8756067a66513a225625422db66e
Bright Doro <brightdoroagrictechventure@gmail.com>
2025-05-24 23:50:36 +0100

    Initial UI setup for smart-farming-platform

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
 console.log("Smart Farming Platform.");
-// This script is part of a smart farming platform that integrates IoT devices for monitoring and managing agricultural activities.
+// This script is part of a smart farming platform that integrates IoT devices for monitoring and managing agricultural activities.function alertWelcome() {
+  alert("Welcome to Smart Farming Platform by Agrictechventure68!");
+}
+
+
